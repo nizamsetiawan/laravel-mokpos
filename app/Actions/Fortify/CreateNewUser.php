9259ea90b8584f36c0902c3ff1851842e8e68c17
M. Nizam Setiawan <nizamsetiawan15@gmail.com>
2024-02-17 17:47:03 +0700

    update register

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -29,12 +29,29 @@ class CreateNewUser implements CreatesNewUsers
                 Rule::unique(User::class),
             ],
             'password' => $this->passwordRules(),
+            'phone' => ['required'],
+            'roles' => ['required'],
+            'image' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif', 'max:2048'],
         ])->validate();
+
+        $filename = null;
+        if (isset($input['image'])) {
+            $filename = time() . '.' . $input['image']->extension();
+            $input['image']->storeAs('public/images', $filename);
+        }
 
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
+            'phone' => $input['phone'],
+            'roles' => $input['roles'],
+            'image' => $filename,
         ]);
+ // Add a success message to the session flash data
+ session()->flash('success', 'User created successfully. Please login.');
+
+ // Redirect to the login page
+ return redirect('/');
     }
 }
